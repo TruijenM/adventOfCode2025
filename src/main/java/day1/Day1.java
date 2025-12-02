9000d1f1378ccb01fd04cd9c28e0d6866bb48a39
MichielTr <r0832449@student.thomasmore.be>
2025-12-02 22:06:36 +0100

    day 1 part 1

---
+++ src/main/java/day1/Day1.java
@@ -0,0 +1,45 @@
+package day1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day1 {
+    private final List<String> lines;
+    private int position = 50;
+    private int timesHitZero=0;
+
+
+    public Day1() throws IOException {
+        this.lines = Files.readAllLines(Paths.get("src/resources/dayOneInput"));
+    }
+
+    public int getPassword() {
+        lines.forEach(line -> {
+            if (position == 0) {
+                timesHitZero++;
+            }
+            final var count = Integer.parseInt(line.substring(1));
+            if (line.startsWith("R")) {
+                handleIncrease(count);
+            } else {
+                handleDecrease(count);
+            }
+        });
+        return timesHitZero;
+    }
+
+    public void handleIncrease(final int count) {
+        position+=count;
+        while(position>99){
+            position-=100;
+        }
+    }
+
+    public void handleDecrease(final int count) {
+        position-=count;
+        while(position<0){
+            position+=100;}
+    }
+}
---
+++ src/test/java/day1/Day1Test.java
@@ -0,0 +1,13 @@
+package day1;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+
+public class Day1Test extends TestCase {
+
+    public static void main(String[] args) throws IOException {
+        Day1 day1 = new Day1();
+        System.out.println(day1.getPassword());
+    }
+}
