390be0a852c2e538bdc5ee6f6e5677e8e0121353
MichielTr <r0832449@student.thomasmore.be>
2025-12-05 01:46:57 +0100

    day 2 part 2

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -14,25 +14,49 @@ public class Day2 {
         ranges = Arrays.asList(line.get(0).split(","));
     }
 
-    public long getInvalidSum() {
+    public long getInvalidSumPart1() {
         ranges.forEach(range -> {
             final var stringParts = range.split("-");
-            handleRange(Long.parseLong(stringParts[0]), Long.parseLong(stringParts[1]));
+            handleRangePart1(Long.parseLong(stringParts[0]), Long.parseLong(stringParts[1]));
         });
 
         return invalidIdSum;
     }
 
-    public void handleRange(final long start, final long end) {
+    public void handleRangePart1(final long start, final long end) {
         for (long i = start; i <= end; i++) {
-            final var numberAsInt = String.valueOf(i);
-            final var firstHalf = numberAsInt.substring(0, numberAsInt.length() / 2);
-            final var secondHalf = numberAsInt.substring(numberAsInt.length() / 2);
+            final var numberAsString = String.valueOf(i);
+            final var firstHalf = numberAsString.substring(0, numberAsString.length() / 2);
+            final var secondHalf = numberAsString.substring(numberAsString.length() / 2);
 
             if (firstHalf.equals(secondHalf)) {
                 invalidIdSum += i;
             }
         }
     }
+    public long getInvalidSumPart2() {
+        ranges.forEach(range -> {
+            final var stringParts = range.split("-");
+            handleRangePart2(Long.parseLong(stringParts[0]), Long.parseLong(stringParts[1]));
+        });
+        return invalidIdSum;
+    }
+
+    public void handleRangePart2(final long start, final long end) {
+        for (long i = start; i <= end; i++) {
+            final var numberAsString = String.valueOf(i);
+
+            for (int j = 0; j <= numberAsString.length()/2; j++) {
+                    final var partialString = numberAsString.substring(0, j);
+                    final var regexString = "^(" + partialString + ")+$";
+                    if (numberAsString.matches(regexString)) {
+                        invalidIdSum += i;
+                        break;
+                }
+            }
+        }
+    }
+
+
 
 }
